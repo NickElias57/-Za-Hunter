cf726c4dbb0948a31b5beb5d3308ae82318b2b51
nick <nickelias57@yahoo.com>
2021-03-09 15:31:00 -0600

    Added map search feature

--- 'Za Hunter/ContentView.swift
+++ 'Za Hunter/ContentView.swift
@@ -9,36 +9,79 @@ import SwiftUI
 import MapKit
 
 struct ContentView: View {
+    @State private var places = [Place]()
     @StateObject var locationManager = LocationManager()
     @State private var userTrackingMode: MapUserTrackingMode = .follow
     @State private var region = MKCoordinateRegion(
-
+        
         center: CLLocationCoordinate2D(
-
+            
             latitude: 42.15704,
-
+            
             longitude: -88.14812),
-
+        
         span: MKCoordinateSpan(
-
+            
             latitudeDelta: 0.05,
-
+            
             longitudeDelta: 0.05)
-
+        
     )
-
+    
     var body: some View {
         Map(
 
-                    coordinateRegion: $region,
+                 coordinateRegion: $region,
 
-                    interactionModes: .all,
+                 interactionModes: .all,
 
-                    showsUserLocation: true,
+                 showsUserLocation: true,
 
-                    userTrackingMode: $userTrackingMode
+                 userTrackingMode: $userTrackingMode,
 
-                )
+                 annotationItems: places) { place in
+
+                 MapPin(coordinate: place.annotation.coordinate)
+
+             }
+
+             .onAppear(perform: {
+
+                 performSearch(item: "Pizza")
+
+             })
+
+
+
+    }
+    func performSearch(item: String) {
+        let searchRequest = MKLocalSearch.Request()
+        
+        searchRequest.naturalLanguageQuery = item
+        
+        searchRequest.region = region
+        
+        let search = MKLocalSearch(request: searchRequest)
+        
+        search.start { (response, error) in
+            if let response = response {
+                
+                for mapItem in response.mapItems {
+                    
+                    let annotation = MKPointAnnotation()
+                    
+                    annotation.coordinate = mapItem.placemark.coordinate
+                    
+                    annotation.title = mapItem.name
+                    
+                    places.append(Place(annotation: annotation, mapItem: mapItem))
+                    
+                }
+                
+            }
+        }
+        
+        
     }
 }
 
@@ -46,4 +89,13 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+struct Place: Identifiable {
+    
+    let id = UUID()
+    
+    let annotation: MKPointAnnotation
+    
+    let mapItem: MKMapItem
+    
 }
